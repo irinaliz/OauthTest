7b19ff3d0a4621712abaecbe87ceb847b067275e
Bomin Lee <62278499+irinaliz@users.noreply.github.com>
2021-12-10 00:24:24 +0900

    Update KakaoBO.java

--- KakaoBO.java
+++ KakaoBO.java
@@ -6,19 +6,17 @@ import java.net.URLEncoder;
 public class KakaoBO {
 	
 	private String client_id;
-	private String client_pw;
 	private String callbackURL;
 	private String apiURL;
 	
 	public KakaoBO() throws UnsupportedEncodingException {
 		this.client_id = "Your Clinet Key"; //JavaScript Key ( App key )
-		this.client_pw = "5SV_f1VAKg";
 		this.callbackURL = URLEncoder.encode("http://localhost:8090/kakaoCallback.do","UTF-8");
 		this.apiURL = "https://kauth.kakao.com/oauth/authorize?"+
 						"client_id=" + client_id +
 						"&redirect_uri=" +callbackURL +
 						"&response_type=code";
-				//+"&state=";
+				
 	}
 	
 	public String getClient_id() {
@@ -30,9 +28,6 @@ public class KakaoBO {
 	}
 	public String getApiURL() {
 		return apiURL;
-	}
-	public String getClient_pw() {
-		return client_pw;
 	}
 
 /*	https://kauth.kakao.com/oauth/authorize
